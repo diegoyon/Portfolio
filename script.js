d9ae7f0588ddaf436d8bd7605e86ed9491c12208
diego <diegoyon@outlook.com>
2022-06-13 15:02:36 -0600

    Add styles and correctly anchor links

--- script.js
+++ script.js
@@ -10,16 +10,26 @@ function displaymenu() {
 
     let iconX = document.createElement('img');
     iconX.src='./images/IconX.png';
+    iconX.className = 'iconx';
     iconX.addEventListener('click', ()=>{body.removeChild(menu)});
 
-    let portfolio = document.createElement('h2');
+    let portfolio = document.createElement('a');
+    portfolio.href='#portfoliosection'
     portfolio.textContent = "Portfolio";
+    portfolio.className = 'menu-items';
+    portfolio.addEventListener('click', ()=>{body.removeChild(menu)});
 
-    let aboutme = document.createElement('h2');
-    aboutme.textContent = "Aboutme";
+    let aboutme = document.createElement('a');
+    aboutme.href='#aboutmesection'
+    aboutme.textContent = "About";
+    aboutme.className = 'menu-items';
+    aboutme.addEventListener('click', ()=>{body.removeChild(menu)});
 
-    let contact = document.createElement('h2');
+    let contact = document.createElement('a');
+    contact.href='#contactsection'
     contact.textContent = "Contact";
+    contact.className = 'menu-items';
+    contact.addEventListener('click', ()=>{body.removeChild(menu)});
   
     menu.appendChild(iconX);
     menu.appendChild(portfolio);
