1fa82b4f362e2d7ca7bf9b4b394ae332b009d692
diego <diegoyon@outlook.com>
2022-06-13 15:09:01 -0600

    Fix linters errors

--- script.js
+++ script.js
@@ -1,40 +1,39 @@
-let mobile = document.querySelector('.mobile img');
-
-mobile.addEventListener('click', displaymenu);
-
-let body = document.querySelector('body');
+const mobile = document.querySelector('.mobile img');
+const body = document.querySelector('body');
 
 function displaymenu() {
-    let menu = document.createElement('div');
-    menu.className = 'menu-attributes';
+  const menu = document.createElement('div');
+  menu.className = 'menu-attributes';
 
-    let iconX = document.createElement('img');
-    iconX.src='./images/IconX.png';
-    iconX.className = 'iconx';
-    iconX.addEventListener('click', ()=>{body.removeChild(menu)});
+  const iconX = document.createElement('img');
+  iconX.src = './images/IconX.png';
+  iconX.className = 'iconx';
+  iconX.addEventListener('click', () => { body.removeChild(menu); });
 
-    let portfolio = document.createElement('a');
-    portfolio.href='#portfoliosection'
-    portfolio.textContent = "Portfolio";
-    portfolio.className = 'menu-items';
-    portfolio.addEventListener('click', ()=>{body.removeChild(menu)});
+  const portfolio = document.createElement('a');
+  portfolio.href = '#portfoliosection';
+  portfolio.textContent = 'Portfolio';
+  portfolio.className = 'menu-items';
+  portfolio.addEventListener('click', () => { body.removeChild(menu); });
 
-    let aboutme = document.createElement('a');
-    aboutme.href='#aboutmesection'
-    aboutme.textContent = "About";
-    aboutme.className = 'menu-items';
-    aboutme.addEventListener('click', ()=>{body.removeChild(menu)});
+  const aboutme = document.createElement('a');
+  aboutme.href = '#aboutmesection';
+  aboutme.textContent = 'About';
+  aboutme.className = 'menu-items';
+  aboutme.addEventListener('click', () => { body.removeChild(menu); });
 
-    let contact = document.createElement('a');
-    contact.href='#contactsection'
-    contact.textContent = "Contact";
-    contact.className = 'menu-items';
-    contact.addEventListener('click', ()=>{body.removeChild(menu)});
-  
-    menu.appendChild(iconX);
-    menu.appendChild(portfolio);
-    menu.appendChild(aboutme);
-    menu.appendChild(contact);
+  const contact = document.createElement('a');
+  contact.href = '#contactsection';
+  contact.textContent = 'Contact';
+  contact.className = 'menu-items';
+  contact.addEventListener('click', () => { body.removeChild(menu); });
 
-    body.appendChild(menu);
+  menu.appendChild(iconX);
+  menu.appendChild(portfolio);
+  menu.appendChild(aboutme);
+  menu.appendChild(contact);
+
+  body.appendChild(menu);
 }
+
+mobile.addEventListener('click', displaymenu);
